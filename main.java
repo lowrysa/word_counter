ba497b74ffdcb602138bbc76c637651276b3283b
Sam Lowry <lowrysa@email.sc.edu>
2021-08-06 14:55:35 -0400

    added comments

--- main.java
+++ main.java
@@ -3,10 +3,12 @@ import java.io.*;
 public class main {
     private static Scanner k;
     public static final void main (String args[]) {
+        //Run Program
         k = new Scanner(System.in);
         run();
     }
 
+    //Meat and Potatoes
     private static void run() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
@@ -15,7 +17,7 @@ public class main {
         while (!finished) {
             System.out.println("Either input text, or a .txt file to be counted:");
             String input = k.nextLine();
-            try {
+            try { //If file exists, it will be read thru
                 Scanner fileReader = new Scanner(new File(input));
                 System.out.println("\nOpening file...");
                 String inputFile = "";
@@ -25,7 +27,7 @@ public class main {
                 System.out.println("Word Count: " + splittingLine.length);
                 fileReader.close();
                 finished = true;
-            } catch (FileNotFoundException ex) {
+            } catch (FileNotFoundException ex) { //Input must not be file then
                 String[] splittingLine = input.split(" ");
                 System.out.println("\nWord Count: " + splittingLine.length);
                 finished = true;
